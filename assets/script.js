4921e054d7d04a445167f2f2cca1cef9fb0dded6
awjcodes <146032624+awjcodes@users.noreply.github.com>
2023-11-03 01:57:46 -0700

    Right & Wrong Answer Displayed ADDED

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,6 @@ var startQuizContainer = document.querySelector('#quiz_style_container');
 var questionOne = document.querySelector('#question_1');
 
 
-
 // USER CLICKS "START QUIZ" & IS PRESENTED BY THE FIRST QUESTION
 function startQuiz() {
     startBtn.addEventListener('click', function () {
@@ -13,52 +12,64 @@ function startQuiz() {
 }
 startQuiz();
 
+//Target All Wrong Classes and making it showResult
 
 
+// Displays whether User's Answer was RIGHT or WRONG when they click on an ANSWER
+//The result of their answer showing and hiding for a certain amount of time
+function showResultRight() {
+    var resultEl = document.createElement('h4');
+    resultEl.innerText = "Right";
+    document.body.appendChild(resultEl);
+    resultEl.style.display = 'flex'
+    resultEl.style.justifyContent = 'center'
+    resultEl.style.alignContent = 'center'
+    resultEl.style.marginTop = '2vh'
+    resultEl.style.fontSize = '3rem'
 
+    setTimeout(function() {
+        document.body.removeChild(resultEl);
+    }, 500);
+}
+
+
+function showResultWrong() {
+    var resultEl = document.createElement('h4');
+    resultEl.innerText = "Wrong";
+    document.body.appendChild(resultEl);
+    resultEl.style.display = 'flex'
+    resultEl.style.justifyContent = 'center'
+    resultEl.style.alignContent = 'center'
+    resultEl.style.marginTop = '2vh'
+    resultEl.style.fontSize = '3rem'
+
+    setTimeout(function() {
+        document.body.removeChild(resultEl);
+    }, 500);
+}
 
 
 //Question 1 to Question 2
 var questionTwo = document.querySelector('#question_2');
-var clickAnswer1 = document.querySelectorAll('.answer_1');
+var clickAnswer1Right = document.querySelectorAll('.answer_1right');
+var clickAnswer1Wrong = document.querySelectorAll('.answer_1wrong');
 
 
 function showQuestion2() {
     questionOne.style.display = "none";
     questionTwo.style.display = "flex";
-}
+};
 
-clickAnswer1.forEach(function (answer1) {
+// DISPLAYS WHETHER USER CHOICE OF ANSWER IS RIGHT OR WRONG
+clickAnswer1Right.forEach(function (answer1) {
     answer1.addEventListener('click', showQuestion2);
+    answer1.addEventListener('click', showResultRight);
 });
 
-
-
-
-
-
-
-
-
-// Displays whether User's Answer was RIGHT or WRONG when they click on an ANSWER
-
-function showResultRight() {
-    var resultEl = document.createElement('h3');
-    resultEl.innerText = "Right";
-    document.body,appendChild(resultEl);
-}
-
-function showResultWrong() {
-    var resultEl = document.createElement('h3');
-    resultEl.innerText = "Wrong";
-    document.body,appendChild(resultEl);
-}
-
-
-
-
-
-
+clickAnswer1Wrong.forEach(function (answer1) {
+    answer1.addEventListener('click', showQuestion2);
+    answer1.addEventListener('click', showResultWrong);
+});
 
 
 
@@ -71,16 +82,24 @@ function showResultWrong() {
 
 //Question 2 to Question 3
 var questionThree = document.querySelector('#question_3')
-var clickAnswer2 = document.querySelectorAll('.answer_2')
+var clickAnswer2Right = document.querySelectorAll('.answer_2right')
+var clickAnswer2Wrong = document.querySelectorAll('.answer_2wrong')
 
 
 function showQuestion3() {
     questionTwo.style.display = "none"
     questionThree.style.display = "flex"
-}
+};
 
-clickAnswer2.forEach(function (answer2) {
+// DISPLAYS WHETHER USER CHOICE OF ANSWER IS RIGHT OR WRONG
+clickAnswer2Right.forEach(function (answer2) {
     answer2.addEventListener('click', showQuestion3);
+    answer2.addEventListener('click', showResultRight);
+});
+
+clickAnswer2Wrong.forEach(function (answer2) {
+    answer2.addEventListener('click', showQuestion3);
+    answer2.addEventListener('click', showResultWrong);
 });
 
 
@@ -94,15 +113,24 @@ clickAnswer2.forEach(function (answer2) {
 
 //Question 3 to Question 4
 var questionFour = document.querySelector('#question_4')
-var clickAnswer3 = document.querySelectorAll('.answer_3')
+var clickAnswer3Right = document.querySelectorAll('.answer_3right')
+var clickAnswer3Wrong = document.querySelectorAll('.answer_3wrong')
+
 
 function showQuestion4() {
     questionThree.style.display = "none"
     questionFour.style.display = "flex"
-}
+};
 
-clickAnswer3.forEach(function (answer3) {
+// DISPLAYS WHETHER USER CHOICE OF ANSWER IS RIGHT OR WRONG
+clickAnswer3Right.forEach(function (answer3) {
     answer3.addEventListener('click', showQuestion4);
+    answer3.addEventListener('click', showResultRight);
+});
+
+clickAnswer3Wrong.forEach(function (answer3) {
+    answer3.addEventListener('click', showQuestion4);
+    answer3.addEventListener('click', showResultWrong);
 });
 
 
@@ -116,15 +144,24 @@ clickAnswer3.forEach(function (answer3) {
 
 //Question 4 to Question 5
 var questionFive = document.querySelector('#question_5')
-var clickAnswer4 = document.querySelectorAll('.answer_4')
+var clickAnswer4Right = document.querySelectorAll('.answer_4right')
+var clickAnswer4Wrong = document.querySelectorAll('.answer_4wrong')
+
 
 function showQuestion5() {
     questionFour.style.display = "none"
     questionFive.style.display = "flex"
-}
+};
 
-clickAnswer4.forEach(function (answer4) {
+// DISPLAYS WHETHER USER CHOICE OF ANSWER IS RIGHT OR WRONG
+clickAnswer4Right.forEach(function (answer4) {
     answer4.addEventListener('click', showQuestion5);
+    answer4.addEventListener('click', showResultRight);
+});
+
+clickAnswer4Wrong.forEach(function (answer4) {
+    answer4.addEventListener('click', showQuestion5);
+    answer4.addEventListener('click', showResultWrong);
 });
 
 
@@ -136,16 +173,24 @@ clickAnswer4.forEach(function (answer4) {
 
 //Question 5 to End Screen
 var endScreen = document.querySelector('#end_screen')
-var clickAnswer5 = document.querySelectorAll('.answer_5')
+var clickAnswer5Right = document.querySelectorAll('.answer_5right')
+var clickAnswer5Wrong = document.querySelectorAll('.answer_5wrong')
 
 function showEndScreen() {
     questionFive.style.display = "none"
     endScreen.style.display = "flex"
-}
+};
 
-clickAnswer5.forEach(function (answer5) {
-    answer5.addEventListener('click', showEndScreen)
-})
+// DISPLAYS WHETHER USER CHOICE OF ANSWER IS RIGHT OR WRONG
+clickAnswer5Right.forEach(function (answer5) {
+    answer5.addEventListener('click', showEndScreen);
+    answer5.addEventListener('click', showResultRight);
+});
+
+clickAnswer5Wrong.forEach(function (answer5) {
+    answer5.addEventListener('click', showEndScreen);
+    answer5.addEventListener('click', showResultWrong);
+});
 
 
 
